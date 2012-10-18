8f7a70ba2236f326dc8ec590c487e2b6774322d4
kumarpiyush <piyush5kumar5@gmail.com>
2012-10-19 00:14:45 +0530

    first day final

--- foobar.sql
+++ foobar.sql
@@ -16,10 +16,67 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `bill_details`
+--
+CREATE DATABASE IF NOT EXISTS foobar;
+USE foobar;
+DROP TABLE IF EXISTS `bill_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill_details` (
+  `bill_id` int(11) NOT NULL DEFAULT '0',
+  `prod_type` varchar(30) NOT NULL DEFAULT '',
+  `prod_id` int(11) NOT NULL DEFAULT '0',
+  `quantity` int(11) DEFAULT NULL,
+  `cost` int(11) DEFAULT NULL COMMENT 'cost is per piece',
+  PRIMARY KEY (`bill_id`,`prod_id`,`prod_type`),
+  KEY `prod_type` (`prod_type`),
+  CONSTRAINT `bill_details_ibfk_1` FOREIGN KEY (`bill_id`) REFERENCES `billing` (`ID`),
+  CONSTRAINT `bill_details_ibfk_2` FOREIGN KEY (`prod_type`) REFERENCES `categories` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill_details`
+--
+
+LOCK TABLES `bill_details` WRITE;
+/*!40000 ALTER TABLE `bill_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `billing`
+--
+
+DROP TABLE IF EXISTS `billing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `billing` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `total_cost` int(11) DEFAULT NULL,
+  `bill_date` date DEFAULT NULL,
+  `payment_mode` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `billing_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `billing`
+--
+
+LOCK TABLES `billing` WRITE;
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `book`
 --
-CREATE DATABASE IF NOT EXISTS foobar; 
-USE foobar;
+
 DROP TABLE IF EXISTS `book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -43,6 +100,29 @@ CREATE TABLE `book` (
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES ('book'),('clothing'),('computer_accessories'),('electronics');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -168,4 +248,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-10-18 23:27:44
+-- Dump completed on 2012-10-19  0:12:53
