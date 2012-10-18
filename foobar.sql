b77e3f06208c3d04e2380e7642330f5a8a92712c
piyush <piyush@piyush.(none)>
2012-10-18 22:20:18 +0530

    database dump :: has categories, customer

---
+++ foobar.sql
@@ -0,0 +1,170 @@
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: foobar
+-- ------------------------------------------------------
+-- Server version	5.5.24-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `category` varchar(30) NOT NULL,
+  `mrp` float NOT NULL,
+  `price` float NOT NULL,
+  `description` text,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clothing`
+--
+
+DROP TABLE IF EXISTS `clothing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clothing` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(20) DEFAULT NULL COMMENT 'men/women/children',
+  `category2` varchar(20) DEFAULT NULL COMMENT 'location of wear',
+  `size` varchar(5) DEFAULT NULL COMMENT 'size of product',
+  `mrp` float NOT NULL,
+  `price` float NOT NULL,
+  `description` text,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clothing`
+--
+
+LOCK TABLES `clothing` WRITE;
+/*!40000 ALTER TABLE `clothing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clothing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `computer_accessories`
+--
+
+DROP TABLE IF EXISTS `computer_accessories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `computer_accessories` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(100) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `mrp` float NOT NULL,
+  `price` float NOT NULL,
+  `description` text,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `computer_accessories`
+--
+
+LOCK TABLES `computer_accessories` WRITE;
+/*!40000 ALTER TABLE `computer_accessories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `computer_accessories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `address` text,
+  `acct_no` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `electronics`
+--
+
+DROP TABLE IF EXISTS `electronics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `electronics` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(100) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `mrp` float NOT NULL,
+  `price` float NOT NULL,
+  `description` text,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `electronics`
+--
+
+LOCK TABLES `electronics` WRITE;
+/*!40000 ALTER TABLE `electronics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `electronics` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-10-18 22:19:37
