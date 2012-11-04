9302033ad2b2732b71f6c007a9857532ee5947e2
kumarpiyush <piyush5kumar5@gmail.com>
2012-11-05 00:43:56 +0530

    book updated

--- data_gen/book.py
+++ data_gen/book.py
@@ -1,10 +1,11 @@
 import random
+import commands
 def main():
     alpha=open("book.dat","r")
     data=alpha.readlines()
     alpha.close()
 
-    catset=["fiction","biography","science","drama","poetry","kids"]
+    catset=["fiction","health","kids","biography","science"]
     set1=["this is a good book on ","this is a masterpiece on ","this book talks about ","there are breathtaking descriptions of "]
     for line in data:
         line=line.split("\t")
@@ -13,8 +14,14 @@ def main():
         line[5]=line[4]-random.randint(0,50)              # price
         line[6]=set1[random.randint(0,len(set1)-1)]+line[3]+" by "+line[2]   # description
         line[7]=random.randint(1000,2000)
-        line[8]=0   # book link
-        print line
+        
+        path="/home/piyush/Database-Project/NetBeans-source/Foobar/web/images/book/"+line[3]
+        x = commands.getoutput("ls "+path+" -l| wc -l")
+        y = int(x) - 1
+        line+=["images/book/"+line[3]+"/"+`random.randint(0,y-1)`+".jpeg"]
+        for i in line:
+            print i,"\t",
+        print "\n"
 
 if __name__=="__main__":
     main()
