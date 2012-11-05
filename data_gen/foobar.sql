2a343ca2f212b3dc903de561d149829b9c514824
kumarpiyush <piyush5kumar5@gmail.com>
2012-11-05 19:00:57 +0530

    foobar updated

--- data_gen/foobar.sql
+++ data_gen/foobar.sql
@@ -91,7 +91,7 @@ CREATE TABLE `book` (
   `img_url` text,
   `popularity` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=23887 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23202 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -254,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-11-05 10:19:16
+-- Dump completed on 2012-11-05 19:00:35
